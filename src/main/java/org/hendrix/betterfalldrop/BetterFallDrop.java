32a144436f51700e202453b5001e5568153400a5
JimiIT92 <14943331+JimiIT92@users.noreply.github.com>
2026-07-16 14:33:44 +0200

    Initial commit

---
+++ src/main/java/org/hendrix/betterfalldrop/BetterFallDrop.java
@@ -0,0 +1,24 @@
+package org.hendrix.betterfalldrop;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * Better Fall Drop.
+ * WIP
+ */
+public final class BetterFallDrop implements ModInitializer {
+
+    /**
+     * The {@link String Mod ID}
+     */
+    public static final String MOD_ID = "betterfalldrop";
+
+    /**
+     * Initialize the mod
+     */
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
